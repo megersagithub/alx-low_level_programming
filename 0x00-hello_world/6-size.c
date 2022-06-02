3b69a03d2fbe00303369008f82167c0b5556f0ae
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-02 14:32:24 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,13 @@
 /**
  * main: the starting point
  * Return: returns 0 for 0s
- * description: A program prints a atring a size of variable types
+ * Description: A program prints a atring a size of variable types
  **/
 int main(void)
 {
-	printf("Size of char : %zu byte(s)\n", sizeof(char));
-	printf("Size of int : %zu byte(s)\n", sizeof(int));
-	printf("Size of long int : %zu byte(s)\n", sizeof(long int));
-	printf("Size of long long int : %zu byte(s)\n", sizeof(long long int));
-	printf("Size of float : %zu byte(S)\n", sizeof(float));
+	printf("Size of char : %d byte(s)\n", sizeof(char));
+	printf("Size of int : %d byte(s)\n", sizeof(int));
+	printf("Size of long int : %d byte(s)\n", sizeof(long int));
+	printf("Size of long long int : %d byte(s)\n", sizeof(long long int));
+	printf("Size of float : %d byte(S)\n", sizeof(float));
 }
