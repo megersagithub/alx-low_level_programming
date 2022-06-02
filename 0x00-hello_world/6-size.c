85337a416f17a4d9f6d9b460b17827eea13b6c1c
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-02 14:58:47 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
-#include <studio.h>
+#include<stdio.h>
+
 /**
- * main: the starting point
- * Return: returns 0 for 0s
- * Description: A program prints a atring a size of variable types
+ * main- the starting point
+ * Return: it returns 0 for os
+ * Description: the program prints a string the size of variable types
  **/
+
 int main(void)
 {
-	printf("Size of char : %d byte(s)\n", sizeof(char));
-	printf("Size of int : %d byte(s)\n", sizeof(int));
-	printf("Size of long int : %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int : %d byte(s)\n", sizeof(long long int));
-	printf("Size of float : %d byte(S)\n", sizeof(float));
-	return (0);
+		printf("Size of a char: %zu byte(s)\n", sizeof(char));
+		printf("Size of an int: %zu byte(s)\n", sizeof(int));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %zu byte(s)\n", sizeof(float));
+		return (0);
 }
