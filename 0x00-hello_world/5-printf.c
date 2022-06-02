774c71219623ac841d2579d916953a4da26b97f2
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-02 12:06:42 +0300

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- printing text with printf
- *  
- * Return: returns message
- * Description: description of a code
- */
+ *  * main- printing text with printf
+ *   *
+ *    * Return: returns message
+ *     * Description: description of the code
+ *      */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
