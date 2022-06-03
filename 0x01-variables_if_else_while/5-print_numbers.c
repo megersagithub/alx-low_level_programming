1c3f5f91ea74335482b360888bf89907f27e7e4e
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-03 22:21:53 +0300

    prints all single digit numbers base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- prints numbers
+ * Return: return values
+ * Description: this code prints numbers
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
