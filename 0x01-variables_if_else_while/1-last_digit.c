5265bb91ad744f68dd70d096641c948bf1f5b184
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-03 17:06:00 +0300

    random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,21 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
  * main- generate random number
- * Return: return values
- * Description: conditional
+ * Return: return value
+ * Description: this code is conditional
  */
 int main(void)
 {
-	int n;
+	int n, m;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	m = n % 10;
 	if (m > 5)
-		printf("Last digit %d is %d and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	else
