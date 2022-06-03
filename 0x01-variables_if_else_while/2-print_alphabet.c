a50a6ea3d2461c3efb0af4d548bb085fcfcaff54
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-03 21:51:35 +0300

    print lower case letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- print lower case letter
+ * Return: return alphabet
+ * Description: prints the alphabet in lowercase
+ */
+int main(void)
+{
+	char alphabet;
+
+	fror(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar (alphabet);
+	}
+	putchar ('\n');
+	return (0);
+}
