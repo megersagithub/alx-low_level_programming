edd184a0311d9086fe4706500627310486d581ba
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-08 12:43:38 +0300

    check lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*_islower -  check lower case character.
+* @c: character to check a case
+*
+* Return: Returns 1 if c is lowercase otherwise 1.
+*/
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
