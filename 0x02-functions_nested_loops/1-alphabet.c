e9316f6c8dd1f952f22a580c231bf30d09252631
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-08 12:15:56 +0300

    prints alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * Print-alphabet- Entry point
+ * Return: returns values
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
