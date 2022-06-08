6b09647e7260fad1030148e6fda6baf8d6e6cde9
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-08 12:27:42 +0300

    prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: returns 0
+ */
+int print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
