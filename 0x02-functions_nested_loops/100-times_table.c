546d36c219e58ebffcfdbc934685a672c929ea56
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-08 23:18:22 +0300

    prints n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n times table.
+ *
+ * @n: number times table.
+ *
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int a, b, o;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(48);
+			for (b = 1; b <= n; b++)
+			{
+				o = a + b;
+				_putchar(44);
+				_putchar(32);
+				if (o <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(o + 48);
+				}
+				else if (0 <= 99)
+				{
+					_putchar(32);
+					_putchar((o / 10) + 48);
+					_putchar((o % 10) + 48);
+				}
+				else
+				{
+					_putchar(((o / 100) % 10) + 48);
+					_putchar(((o / 10) % 10) + 48);
+					_putchar((o % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+
+	}
+}
