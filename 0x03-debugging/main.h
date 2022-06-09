746575331bdd33569fd8e027dbdb68f2ddc84f0b
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-09 13:55:15 +0300

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-void positive_or_negative(int i);
+void positive_or_negative(int j);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
