3d16a763e1d4a2c0f0b45d4439daf9e406f9bd81
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-09 23:15:54 +0300

    prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - distiguish prime numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	unsigned int long num = 612852475143, i = (int) sqrt(n);
+	while (1)
+	{
+		if (num % a == 0)
+		{
+			printf("%lu \n", num / i);
+			break;
+		}
+		i--;
+	}
+	return (0);
+}
