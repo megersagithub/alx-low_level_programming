389ec25a234f97c7fc50a45644fafc85cc2b55b9
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-10 00:04:05 +0300

    converts day of year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,32 +6,47 @@
  * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
- * @year: year
- * Return: void
+ * Return: day of the year.
  */
-
-void print_remaining_days(int month, int day, int year)
+int print_remaining_days(int month, int day)
 {
-	if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0))
+	switch (month)
 	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
+		case 2:
+			day = 31 + day;
+			break;
+		case 3:
+			day = 59 + day;
+			break;
+		case 4:
+			day = 90 + day;
+			break;
+		case 5:
+			day = 120 + day;
+			break;
+		case 6:
+			day = 151 + day;
+			break;
+		case 7:
+			day = 181 + day;
+			break;
+		case 8:
+			day = 212 + day;
+			break;
+		case 9:
+			day = 243 + day;
+			break;
+		case 10:
+			day = 273 + day;
+			break;
+		case 11:
+			day = 304 + day;
+			break;
+		case 12:
+			day = 334 + day;
+			break;
+		default:
+			break;
+												}
+	return (day);
 }
