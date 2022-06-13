c6c7d1c5d3c35dd2868bf2695f595927b630e007
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-13 21:41:47 +0300

    copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string pointed by src to pointed by dest.
+ * @src: src string
+ * @dest: destination string
+ *
+ * Return: returns string pointed by dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		*(dest + i) = *(src + i)
+	} while (*(src + i) != '\n')
+	return (dest);
+}
