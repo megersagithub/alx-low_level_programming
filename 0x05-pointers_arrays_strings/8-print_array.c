eecfc758ef33e2598efc9ce511c38cebb96aa986
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-13 21:30:42 +0300

    prints elements of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,16 +7,19 @@
  * @n: number of elements
  *
  * Return: no return.
- */
+*/
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	for (; i < n; i++)
+	i = 0;
+	for (n--; n >= 0; n--, i++)
 	{
-		printf("%d", *(a + i));
-		if (i != (n - 1))
+		printf("%d", a[i]);
+		if (n > 0)
+		{
 			printf(", ");
+		}
 	}
-	printf('\n');
+	printf("\n");
 }
