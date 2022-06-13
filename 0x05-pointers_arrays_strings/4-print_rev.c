8f7d7f554df6b62e99645bcfbb655973af2931bd
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-13 18:24:18 +0300

    prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - prints string in reverse.
+ * @st: string
+ */
+void print_rev(char *st)
+{
+	int i = 1;
+	int len = strlen(st);
+
+	while (len > i)
+	{
+		_putchar(st[len])
+			len--;
+	}
+	_putchar('\n');
+}
