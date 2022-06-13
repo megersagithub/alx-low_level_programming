988955d52a6e212011dc728f30d0de1397987cda
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-13 21:50:29 +0300

    copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,14 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = -1;
+	int i = 0;
 
-	do {
+	while (i >= 0)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
 		i++;
-		dest[i] = src[i];
-	} while (src[i] != '\0')
-
+	}
 	return (dest);
 }
