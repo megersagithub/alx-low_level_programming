d5f9c67704fa33c04aa02cb631b994ccf12319ba
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-16 10:52:56 +0300

    encodes strings into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,16 +10,16 @@ char *leet(char *st)
 {
 	int i, j;
 	char st1[] = "aeotl";
-	char st1[] = "AEOTL";
-	char st2[] = "43071";
+	char st2[] = "AEOTL";
+	char st3[] = "43071";
 
 	for (i = 0; *(st + i) != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (*(st + i) == *(st1 + j) || *(st + i) == *(st1 + j))
+			if (*(st + i) == *(st1 + j) || *(st + i) == *(st2 + j))
 			{
-				*(st + i) = *(st2 + j);
+				*(st + i) = *(st3 + j);
 				break;
 			}
 		}
