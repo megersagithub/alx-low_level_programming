2468ac21065d999a70ceda5818478cbfa2610a2c
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-16 10:36:09 +0300

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,20 +2,20 @@
 
 /**
  * _strcmp - compares two strings.
- * @s1: string 1
- * @s2: string 2
+ * @st1: string 1
+ * @st2: string 2
  *
  * Return: 0 if matching, nstr1 - nstr2 if not matching.
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *st1, char *st2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	for (i = 0; st1[i] != '\0' || st2[i] != '\0'; i++)
 	{
-		if (s1[i] != s2[i])
+		if (st1[i] != st2[i])
 		{
-			return (s1[i] - s2[i]);
+			return (st1[i] - st2[i]);
 		}
 	}
 	return (0);
