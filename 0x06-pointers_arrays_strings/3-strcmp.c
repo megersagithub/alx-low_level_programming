1d11bd937ae3c57fc709baac3bbe586aaa4393fd
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-16 10:34:37 +0300

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _strcimp - compares two strings.
- * @str1: string 1
- * @str2: string 2
+ * _strcmp - compares two strings.
+ * @s1: string 1
+ * @s2: string 2
  *
  * Return: 0 if matching, nstr1 - nstr2 if not matching.
  */
-int _strcmp(char *str1, char *str2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; *(str1 + 1) != '\0' || *(str2 + i) != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (str1[i] != str2[i])
+		if (s1[i] != s2[i])
 		{
-			return (str1[i] - str2[i]);
+			return (s1[i] - s2[i]);
 		}
 	}
 	return (0);
