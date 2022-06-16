553fd7130da5f655834d6e90c92b7d41af59e337
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-16 10:24:56 +0300

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,10 +16,10 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; *(dest + a) != '\0'; a++)
 	;
 
-	for (b = 0; b < n && src[b] != '\0'; b++)
-		dest[a + b] = src[b];
+	for (b = 0; b < n && *(src + b) != '\0'; b++)
+		*(dest(a + b)) = *(src + b);
 
-	dest[a + b] = '\0';
+	*(dest(a + b)) = '\0';
 
 	return (dest);
 }
