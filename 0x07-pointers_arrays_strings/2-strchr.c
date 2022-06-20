adf12ee0672f63f00daf0b32d94306b650e18b5e
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-20 20:25:51 +0300

    character locates in string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,9 +12,9 @@ Char *_strchr(char *st, char c)
 {
 	int i;
 
-	for (i = 0; *(st + i) >= '\0' ; i++)
+	for (i = 0; st[i] >= '\0' ; i++)
 	{
-		if (*(st + i) == c)
+		if (st[i] == c)
 		{
 			return (st + i);
 		}
