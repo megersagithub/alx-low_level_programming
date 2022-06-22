cbe5f35b5f444208b17131d33693778f861d3853
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-22 14:42:12 +0300

    factorial of number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of a given number.
+ * @n: integer n.
+ *
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
