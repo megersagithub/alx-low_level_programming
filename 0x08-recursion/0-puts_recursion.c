4dcf788518b5986de1bd771a7576a358e0fafed5
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-22 15:31:32 +0300

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,23 @@
-#iclude "main.h"
+#include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
+ * _puts_recursion - prints a string followed by a new line.
  * @s: pointer.
  *
- * Retrun: no return.
+ * Return: no return.
  */
+
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	else
-		_putchar('\n');
 }
