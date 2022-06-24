5e311ecb8d6bcf1456ef10d63a94b1321ef85af5
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-24 21:02:13 +0300

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  multiplies two numbers.
+ * @argc: int num.
+ * @argv: list.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("$i\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
