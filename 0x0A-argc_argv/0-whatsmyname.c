4ace49a02507ae68ab3314fedec34408e561aa9b
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-24 20:27:54 +0300

    prints name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - printes name followed by new line.
+ * @argc: int.
+ * @argv: lists.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
