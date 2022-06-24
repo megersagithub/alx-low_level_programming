b007fda780d808a6aa4649d40e01884089976384
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-24 21:04:21 +0300

    multiplies two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,6 +18,6 @@ int main(int argc, char const *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("$i\n", atoi(argv[1]) * atoi(argv[2]));
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
