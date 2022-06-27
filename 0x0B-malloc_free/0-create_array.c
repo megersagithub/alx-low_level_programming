760bf76703b385c52f93eb33519daab92a8f73f5
root <root@DESKTOP-PR51HPI.localdomain>
2022-06-27 14:49:22 +0300

    creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#iclude "main.h"
+
+/**
+ * *create_array - create an array of chars, and
+ * initializes it with a specific char
+ * @size: size of array.
+ * @c: char initialize.
+ *
+ * Return: pointer to tha array or null.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *m = malloc(size);
+
+	if (size == 0 || m == 0)
+		return (0);
+	while (size--)
+		m[size] = c;
+	return (m);
+}
