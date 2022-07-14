fd3a2b0f9e850e0bf16f01c4e05586d5b9f4c544
root <root@DESKTOP-PR51HPI.localdomain>
2022-07-14 12:14:15 +0300

    function that prints message before the execution of main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * before_main - prints message before main function executed
+ */
+void __attribute__ ((constructor)) before_main()
+{
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
+}
