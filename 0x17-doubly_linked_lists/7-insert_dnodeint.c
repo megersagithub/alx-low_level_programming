1107f4f4a9d52afe0134133cca7aa126beb4d469
root <root@DESKTOP-PR51HPI.localdomain>
2022-09-01 10:43:13 +0300

    insert node at given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at specific index
+ * @h: head of linked list
+ * @idx: index of new node
+ * @n: new node value
+ * Return: inserted node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *new;
+
+	dlistint_t *temp = *h;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (!new || !h)
+		return (NULL);
+
+
+	new->n = n;
+	new->next = NULL;
+
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
+			new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+
+	return (NULL);
+}
